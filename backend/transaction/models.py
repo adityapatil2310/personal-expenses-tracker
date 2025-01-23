d06f46f87a6495d6f3f960e548508f51205d37e3
ShubhSarin <shubhsarin08@gmail.com>
2025-01-23 20:54:23 +0530

    added functionality to transaction app with GET POST PUT DELETE requests

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Transactions(models.Model):
+    userId = models.DecimalField(max_digits=5, decimal_places=0, default=0)
+    amount = models.DecimalField(decimal_places=2, max_digits=15)
+    date = models.DateField()
+    text = models.CharField(max_length=500)
+    transType = models.CharField(max_length=3, default='out')
--- backend/transaction/views.py
+++ backend/transaction/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from .models import Transactions
+from .serializer import TransactionSerializer
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+def show_all_transactions(request):
+    transactions = Transactions.objects.all()
+    serializer = TransactionSerializer(transactions, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET', 'POST'])
+def user_transactions(request, userid):
+    if request.method == 'POST':
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'GET':
+        try:
+            transactions = Transactions.objects.get(userId=userid)
+        except Transactions.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = TransactionSerializer(transactions)
+        return JsonResponse(serializer.data)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def transactions(request, id):
+    try:
+        transaction = Transactions.objects.get(id=id)
+    except Transactions.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        serializer = TransactionSerializer(transaction)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TransactionSerializer(transaction, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        transaction.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
