8c3cbdffc8242bbe2a62471d8c55e8a3c7e09a86
ShubhSarin <shubhsarin08@gmail.com>
2025-02-05 10:25:03 +0530

    added recipient and time in transaction

--- backend/transaction/models.py
+++ backend/transaction/models.py
@@ -5,9 +5,11 @@ class Transactions(models.Model):
     userId = models.DecimalField(max_digits=5, decimal_places=0, default=0)
     amount = models.DecimalField(decimal_places=2, max_digits=15)
     date = models.DateField()
+    time = models.TimeField()
     text = models.CharField(max_length=500)
     transType = models.CharField(max_length=3, default='out')
     category = models.CharField(max_length=50, default='other')
+    recipient = models.CharField(max_length=50, default='other')
     
 class Categories(models.Model):
     name = models.CharField(max_length=50)
